0b7c6d7d6d38ac893f90173efadfafde3183f238
Nour Al Salhi <nuraym9998@gmail.com>
2023-01-07 19:49:34 +0200

    navigate dynamic rout by map

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import Link from "next/link"
 
 const ClientPage = () => {
+  const clients = [
+    { id: 'profile', name: 'Profile' },
+    { id: 'twitter', name: 'Twitter' },
+  ];
   return (
-    <div>This Client Page</div>
+    <div>
+      <h1>This Client Page</h1>
+      <ul>
+        {/* <li><Link href='/clients/profile'>Profile</Link></li>
+        <li><Link href='/clients/twitter'>Twitter</Link></li> */}
+        {/* create dynamic link map */}
+        {
+          clients.map(client => (
+            <li key={client.id}>
+              <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
   )
 }
 
