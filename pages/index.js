041cd44b6ca3f13372f57e59ee158fea9f7da181
Nour Al Salhi <nuraym9998@gmail.com>
2023-01-08 08:48:32 +0200

    static site generating

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,28 @@
 import Link from "next/link"
-const HomePage = () => {
+const HomePage = (props) => {
+  const { products } = props
   return (
     <div>
       <h1> The Home Page </h1>
       <ul>
-          <li><Link href="/portfolio">Portfolio</Link></li>
-          <li><Link href="/clients">Clients</Link></li>
+        <li><Link href="/portfolio">Portfolio</Link></li>
+        <li><Link href="/clients">Clients</Link></li>
+      </ul>
+      <hr />
+      <ul>
+        {  products.map(product => (
+          <li key={product.id}> {product.title} </li>
+        ))}
       </ul>
     </div>
   )
 }
-
+export async function getStaticProps() {
+  //always return object has props key
+  return {
+    props: {
+      products: [{ id: '1', title: 'Product 1' }]
+    }
+  }
+}
 export default HomePage
